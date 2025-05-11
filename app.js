cdd6d7ed0cb77fc25a09c1423bcbef066b5d2f28
alejandroIV <alejandroiv177771@gmail.com>
2025-05-11 20:38:10 -0300

    termine el switch y muestro los datos por consola

--- app.js
+++ app.js
@@ -16,6 +16,21 @@ function convertCurrency(pesos){
         pesos= Number(prompt("ingrese la cantidad de pesos a divisar"))
         resultado = convertCurrency(pesos)
         alert(resultado + "USD")
+
+        let recivo = {
+            divisa:"ARS -> USD",
+            ingresado: pesos + "pesos",
+            dolares: resultado.toFixed(2),
+            fecha: new Date().toLocaleDateString()
+        }
+        history.push(recivo)
+
+        case 2:
+            break
+
+        default:
+            alert("opcion invalida")
     }
-    
+    |
+    console.log (history)
 
